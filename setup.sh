a9bc03569d401e9698e46f176851b41ca3277b81
Lyndon Shi <lyshi@microsoft.com>
2020-03-02 11:41:04 -0800

    update setup script to install dependencies

--- setup.sh
+++ setup.sh
@@ -5,3 +5,11 @@ rm ~/.vimrc
 
 ln -s $CWD"/.profile" ~/.profile
 ln -s $CWD"/.vimrc" ~/.vimrc
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install python3-pip
+sudo pip3 install virtualenvwrapper
+
+source ~/.profile
