039885631fa950de1e3c94c7c9517592b1b65d5d
Lyndon Shi <shilyndon@gmail.com>
2020-03-30 09:47:38 -0700

    readd profile but copy instead of symlink

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ CWD=$(pwd)
 rm ~/.profile
 rm ~/.vimrc
 
-ln -s $CWD"/.profile" ~/.profile
+cp $CWD"/.profile" ~/.profile
 ln -s $CWD"/.vimrc" ~/.vimrc
 
 sudo apt-get update
 sudo apt-get upgrade -y
 
 sudo apt-get install python3-pip
-sudo pip3 install virtualenvwrapper
+pip3 install virtualenvwrapper
 
 source ~/.profile
