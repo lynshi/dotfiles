32d57429e386db0247d38477285582a92e0c8d1f
Lyndon Shi <shilyndon@outlook.com>
2023-06-12 23:03:14 -0700

    core for mac

---
+++ mac-setup.sh
@@ -0,0 +1,40 @@
+#!/bin/zsh
+# Sets up some common packages and configuration.
+# All arguments passed to this script are passed to `ansible-playbook`.
+# For example, `./setup.sh python.yaml` eventually runs `ansible-playbook python.yaml`.
+
+# For coloring terminal output.
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0;0m' # no color
+
+set -e
+function end_setup {
+    echo -e "${RED}Set up failed.${NC}"
+    exit 1
+}
+trap end_setup ERR
+
+echo -e "${BLUE}Updating packages...${NC}"
+brew update
+
+pip_installed=$(python3 -m pip -V)
+if [[ $? -ne 0 ]]; then
+    echo -e "${RED}'pip' not installed, aborting!${NC}"
+    exit 1
+fi
+
+echo -e "${BLUE}Installing ansible via pip...${NC}"
+python3 -m pip install --user ansible
+
+echo -e "${BLUE}Adding $HOME/.local/bin to PATH...${NC}"
+export PATH=${PATH}:${HOME}/.local/bin
+
+echo -e "${BLUE}Installing ansible plugins...${NC}"
+ansible-galaxy collection install community.general
+
+echo -e "${BLUE}Running playbooks...${NC}"
+ansible-playbook "$@"
+
+echo -e "${GREEN}Done setting up! Please run \"source ~/.zshrc\" to complete setup.${NC}"
